a603bfe8f4d38212a185996380b486d9122a0984
viqi zevic <vicky.tanzil@gmail.com>
2018-09-03 18:41:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,5 +48,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (!cardNumber) {
+    return 'Sorry, we don\'t have a credit card on file for you.'
+  }
 }
