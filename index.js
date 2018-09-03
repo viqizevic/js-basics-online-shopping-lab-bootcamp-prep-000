d045f98d9a600c680f120e994a5a39e03d4b89b7
viqi zevic <vicky.tanzil@gmail.com>
2018-09-03 18:23:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  cart.push({itemName: item, itemPrice: Math.random()})
+  cart.push({itemName: item, itemPrice: Math.floor(Math.random()*100)})
 }
 
 function viewCart() {
